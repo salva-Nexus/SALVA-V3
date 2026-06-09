3395b504106130928ab2dc2527b84efbd4699e0e
salvanexus <charlieonyii@gmail.com>
2026-06-10 00:50:40 +0100

    Update: Added Unchecked For Gas Efficiency

--- src/Library/SalvaMath.sol
+++ src/Library/SalvaMath.sol
@@ -7,7 +7,9 @@ library SalvaMath {
         pure
         returns (uint256 _amountOut)
     {
-        _amountOut = (_amountIn * _factor) / _exRate;
+        unchecked {
+            _amountOut = (_amountIn * _factor) / _exRate;
+        }
     }
 
     function calculateNGNAmountOut(uint256 _amountIn, uint256 _exRate, uint256 _factor)
@@ -15,7 +17,9 @@ library SalvaMath {
         pure
         returns (uint256 _amountOut)
     {
-        _amountOut = (_amountIn * _exRate) / _factor;
+        unchecked {
+            _amountOut = (_amountIn * _exRate) / _factor;
+        }
     }
 
     function calculateExactNGNAmountIn(uint256 _usdAmountOut, uint256 _exRate, uint256 _factor)
@@ -23,7 +27,9 @@ library SalvaMath {
         pure
         returns (uint256 _amountIn)
     {
-        _amountIn = (_exRate * _usdAmountOut) / _factor;
+        unchecked {
+            _amountIn = (_exRate * _usdAmountOut) / _factor;
+        }
     }
 
     function calculateExactUSDAmountIn(uint256 _ngnAmountOut, uint256 _exRate, uint256 _factor)
@@ -31,7 +37,9 @@ library SalvaMath {
         pure
         returns (uint256 _amountIn)
     {
-        return (_ngnAmountOut * _factor) / _exRate;
+        unchecked {
+            _amountIn = (_ngnAmountOut * _factor) / _exRate;
+        }
     }
 
     function scaleDown(uint256 _amount, uint256 _factor) internal pure returns (uint256) {
